0d392555e10d9c16b3216a25c6df5dfeea5548a3
Yogesh Pant <yogeshpant@Yogeshs-MacBook-Air.local>
2026-02-11 17:46:12 +0530

    reverting to env variables

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,14 @@ import EventCard from "./components/EventCard";
 import ExploreBtn from "./components/ExploreBtn";
 import { IEvent } from "@/database";
 import { cacheLife } from "next/cache";
-import { events } from "@/lib/constants";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const Page = async () => {
   "use cache";
   cacheLife("hours");
-  // const response = await fetch(`${BASE_URL}/api/events`);
-  // const { events } = await response.json();
+  const response = await fetch(`${BASE_URL}/api/events`);
+  const { events } = await response.json();
 
   return (
     <section>
